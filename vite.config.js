e95c8cf939b37d465bbab33021e78b6d78e0fd33
Shuhei Takasugi <shuhei.takasugi@gmail.com>
2024-03-22 21:30:00 +0900

    prettierをかけた

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
-import { defineConfig } from 'vite'
-import banner from 'vite-plugin-banner';
-import pkg from "./package.json" assert { type: "json" }
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import banner from "vite-plugin-banner";
+import pkg from "./package.json" assert { type: "json" };
+import { resolve } from "path";
 
-const license = ['/*!\n',
-  ` * Start Bootstrap - ${ pkg.title } v${ pkg.version } (${ pkg.homepage })\n`,
-  ` * Copyright 2013-` + (new Date()).getFullYear(), ` ${ pkg.author }\n`,
-  ` * Licensed under ${ pkg.license } (https://github.com/BlackrockDigital/${ pkg.name }/blob/master/LICENSE)\n`,
+const license = [
+  "/*!\n",
+  ` * Start Bootstrap - ${pkg.title} v${pkg.version} (${pkg.homepage})\n`,
+  ` * Copyright 2013-` + new Date().getFullYear(),
+  ` ${pkg.author}\n`,
+  ` * Licensed under ${pkg.license} (https://github.com/BlackrockDigital/${pkg.name}/blob/master/LICENSE)\n`,
   ` */\n`,
-  `\n`
-].join('');
+  `\n`,
+].join("");
 
 export default defineConfig({
   build: {
-    outDir: './', 
+    outDir: "./",
     rollupOptions: {
       input: {
-        ['creative']: resolve(__dirname, 'js/creative.js'),
-        ['creative.css']: resolve(__dirname, 'css/creative.css'),
+        ["creative"]: resolve(__dirname, "js/creative.js"),
+        ["creative.css"]: resolve(__dirname, "css/creative.css"),
       },
-      output: { 
+      output: {
         banner: license,
         entryFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`,
       },
     },
   },
-  plugins: [banner(license)]
-})
+  plugins: [banner(license)],
+});
